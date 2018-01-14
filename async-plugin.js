3d74c01d292ade3e8d2f23997543deac244aeaeb
Luke Edwards <luke.edwards05@gmail.com>
2018-01-13 19:40:47 -0800

    update babel config mutations

--- async-plugin.js
+++ async-plugin.js
@@ -1,20 +1,13 @@
 export default function asyncPlugin(config) {
-	let babel = config.module.loaders.filter( loader => loader.loader === 'babel-loader')[0].options;
+	let babel = config.module.loaders.filter(loader => loader.loader === 'babel-loader')[0].options;
 
 	// Blacklist regenerator within env preset:
 	babel.presets[0][1].exclude.push(
-		'transform-async-to-generator',
-		'transform-regenerator'
+		'transform-async-to-generator'
 	);
 
-	// Replace stage-1 preset with an inlined, flattened version without regenerator:
-	babel.presets.pop();
-	babel.plugins.push(
-		'transform-export-extensions',
-		'syntax-dynamic-import',
-		'transform-class-properties',
-		'transform-object-rest-spread'
-	);
+	// Add missing plugin syntax, until new patch for (developit/preact-cli#461)
+	babel.plugins.push('syntax-dynamic-import');
 
 	// Add Kneden
 	babel.plugins.push(require.resolve('babel-plugin-async-to-promises'));
